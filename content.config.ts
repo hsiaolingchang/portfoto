430adc6bc8e19da2fe018ff331e4ef4e04fd1550
SeanGau <rrtw0627@gmail.com>
2025-01-16 14:16:17 +0800

    fix bug

--- content.config.ts
+++ content.config.ts
@@ -1,5 +1,12 @@
 import { defineContentConfig, defineCollection, z } from '@nuxt/content'
-import { contentDir } from './nuxt.config'
+import { resolve } from 'path'
+import { existsSync } from 'fs'
+
+const contentDir = () => {
+  const contentPath = resolve(__dirname, 'content')
+  const examplePath = resolve(__dirname, 'content.example')
+  return existsSync(contentPath) ? contentPath : examplePath
+}
 
 export default defineContentConfig({
   collections: {
